949d46c8f46a833c0d06391b05397affead38cea
Allison Ause <allison.ause@gmail.com>
2022-09-12 16:28:43 -0700

    added styling to main and incorporated header component with styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <span className="title">Explore the Zodiac</span>
+    </div>
+  );
+}
--- src/components/Zodiac/Zodiac.js
+++
@@ -1,9 +0,0 @@
-export default function Zodiac({ name, dates }) {
-  return (
-    <div className="zodiac-card">
-      <img src={`${process.env.PUBLIC_URL}/zodiacs/${name}`} />
-      <span className="name">{name}</span>
-      <span className="dates">{dates}</span>
-    </div>
-  );
-}
