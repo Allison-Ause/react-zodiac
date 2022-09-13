a79f8ce83fbf801a5e4f7964a37f4ef54d2e7b7a
Allison Ause <allison.ause@gmail.com>
2022-09-13 15:10:30 -0700

    added image to header and adjusted styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <div className="header">
       <span className="title">Explore the Zodiac!</span>
+      <img src={`${process.env.PUBLIC_URL}/header-cat.png`} />
     </div>
   );
 }
