eb2554ab4653acc7841ea33c8062e91a3f9d5fe8
Allison Ause <allison.ause@gmail.com>
2022-09-15 14:20:42 -0700

    added personal notes for image usage reference

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,6 +6,8 @@ import background from '../../background.jpeg';
 export default function Main() {
   return (
     <main className="zodiac-list" style={{ backgroundImage: `url(${background})` }}>
+      {/* When we import the background, it must be in the src file and we don't need to write
+      any of the path after the url because the import brings the entire path.  */}
       {zodiacSigns.map((sign) => (
         <ZodiacCard key={sign.id} {...sign} />
       ))}
--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -3,7 +3,8 @@ import './ZodiacCard.css';
 export default function ZodiacCard({ name, dates }) {
   return (
     <div className="zodiac-card">
-      <img src={`${process.env.PUBLIC_URL}/zodiacs/${name}.png`} />
+      <img src={`${process.env.PUBLIC_URL}/zodiacs/${name}.png`} /> 
+      {/* This img src needs the entire path because we are not importing it. */}
       <span className="name">{name[0].toUpperCase() + name.slice(1)}</span>
       <span className="dates">{dates}</span>
     </div>
